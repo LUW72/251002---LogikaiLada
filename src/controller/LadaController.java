b8d5b402b9cab701ad1f12a59660d227229bd2db
Vajkyy <vajki2014@gmail.com>
2025-10-02 15:05:12 +0200

    LadaController--alapok

--- src/controller/LadaController.java
+++ src/controller/LadaController.java
@@ -1,7 +1,21 @@
 package controller;
 
+import model.LadaModel;
+import view.LadaView;
+
 
 public class LadaController 
 {
+    private LadaModel model;
+    private LadaView view;
+
+    public LadaController(LadaModel model, LadaView view) {
+        this.model = model;
+        this.view = view;
+    }
+    
+    public void feladat(){
+        
+    }
     
 }
